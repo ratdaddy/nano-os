d759060c03a60257c41c914a4ebfc17567d8b173
Brian VanLoo <brian.vanloo@gmail.com>
2025-03-30 22:50:51 -0700

    Create proper boot image and preliminary dump of dtb

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ TARGET := riscv64imac-unknown-none-elf
 BUILD_DIR := target/$(TARGET)/debug
 KERNEL_ELF := $(BUILD_DIR)/kernel
 KERNEL_BIN := $(BUILD_DIR)/kernel.bin
+BOOT_SD := $(BUILD_DIR)/boot.sd
+BOOT_ITS := $(BUILD_DIR)/boot.its
 
 SD_MOUNT=/Volumes/KERNEL
 
@@ -11,15 +13,21 @@ SOURCES := $(shell find src -name '*.rs') Cargo.toml Cargo.lock link.ld
 
 .PHONY: all copy clean
 
-all: $(KERNEL_BIN)
+all: $(BOOT_SD)
 
 $(KERNEL_BIN): $(SOURCES)
 	cargo build --target $(TARGET)
 	$(OBJCOPY) --binary-architecture=riscv64 --strip-all -O binary $(KERNEL_ELF) $(KERNEL_BIN)
 
+$(BOOT_SD): $(KERNEL_BIN) bootdata/boot.its
+	cp bootdata/boot.its $(BUILD_DIR)
+	cp bootdata/sg2002.dtb $(BUILD_DIR)
+	mkimage -f $(BOOT_ITS) $(BOOT_SD) > /dev/null 2>&1
+
 copy: all
 	@echo "Copying $(KERNEL_BIN) to $(SD_MOUNT)..."
-	cp $(KERNEL_BIN) $(SD_MOUNT)/
+	cp $(BOOT_SD) $(SD_MOUNT)/
+	cp bootdata/fip.bin $(SD_MOUNT)/
 	sync
 	@diskutil eject "$$(diskutil info $(SD_MOUNT) | awk -F: '/Device Node/ {gsub(/^[ \t]+/, "", $$2); print $$2}' | sed 's/s[0-9]*$$//')"
 	@echo "Done."
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod console;
 use core::panic::PanicInfo;
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start(hart_id: usize, dtb_ptr: *const u8) -> ! {
     extern "C" {
         static _stack_end: u8;
     }
@@ -22,10 +22,10 @@ pub extern "C" fn _start() -> ! {
         );
     }
 
-    rust_main();
+    rust_main(hart_id, dtb_ptr);
 }
 
-fn rust_main() -> ! {
+fn rust_main(hart_id: usize, dtb_ptr: *const u8) -> ! {
     console::sbi_console_putchar(b'*');
     console::sbi_console_putchar(b'*');
     console::sbi_console_putchar(b'*');
@@ -33,8 +33,39 @@ fn rust_main() -> ! {
 
     println!("Hello, world!");
 
+    println!("Hart ID: {}", hart_id);
+
+    println!("A1 pointer: {:?}", dtb_ptr);
+    hex_dump(dtb_ptr, 512);
+
     loop {
         unsafe { core::arch::asm!("wfi") }
+    }
+}
+
+fn hex_dump(base: *const u8, length: usize) {
+    unsafe {
+        for offset in (0..length).step_by(16) {
+            let line_addr = base.add(offset);
+            print!("{:08p}:", line_addr);
+
+            let mut ascii = [b'.'; 16];
+
+            for i in 0..16 {
+                let addr = base.add(offset + i);
+                let byte = core::ptr::read_volatile(addr);
+                print!(" {:02x}", byte);
+
+                ascii[i] = if byte.is_ascii_graphic() || byte == b' ' {
+                    byte
+                } else {
+                    b'.'
+                };
+            }
+
+            let ascii_str = core::str::from_utf8_unchecked(&ascii);
+            println!("  {}", ascii_str);
+        }
     }
 }
 
